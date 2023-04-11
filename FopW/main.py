3651f1f385fa17acce3abacc448b6dec3f40cf78
vini w <viniciusmoreno.98@gmail.com>
2023-04-11 08:24:38 -0300

    upando codigo

---
+++ FopW/main.py
@@ -0,0 +1,14 @@
+import PyPDF2
+
+with open('0485 - Recibos de Pagamentos 03.2023.pdf', 'rb') as pdf_file:
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    num_pages = len(pdf_reader.pages)
+
+
+    for page_num in range(num_pages):
+        page = pdf_reader.pages[page_num]
+        pdf_writer = PyPDF2.PdfWriter()
+        pdf_writer.add_page(page)
+
+        with open(f'pagina_{page_num+1}.pdf', 'wb') as output_file:
+            pdf_writer.write(output_file)
